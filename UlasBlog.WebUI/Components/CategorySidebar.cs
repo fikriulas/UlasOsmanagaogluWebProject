3bf5f78e37c595c2e550ac0f78e3320138bce212
Fikri Ulaş Osmanağaoğlu <2014010213010@ogrenci.karabuk.edu.tr>
2021-06-13 08:44:02 +0300

    HomePage'e categorySidebar eklendi.

---
+++ UlasBlog.WebUI/Components/CategorySidebar.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UlasBlog.Data.Abstract;
+using UlasBlog.WebUI.Models;
+
+namespace UlasBlog.WebUI.Components
+{
+    public class CategorySidebar : ViewComponent
+    {
+        /*
+         * ana sayfada sağ taraftaki sidebarı temsil eder.
+         * burada 3 adet blog ve tüm kategoriler listeleneceği için blogandcategory modeli
+         * kullanılmıştır. 
+         * */
+        private IUnitOfWork uow;
+        public CategorySidebar(IUnitOfWork _unitOfWork)
+        {
+            uow = _unitOfWork;
+        }
+        public IViewComponentResult Invoke()
+        {
+            // vote'a göre ilk 3 bloğu alır. 
+            var categories = uow.Categories.GetAll();
+            return View(categories);
+        }
+    }
+}
