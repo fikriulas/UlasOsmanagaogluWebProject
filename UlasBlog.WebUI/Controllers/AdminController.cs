ed0c37fe21839c8d5f108b3508b6a42c2b81dac9
Fikri Ulaş Osmanağaoğlu <2014010213010@ogrenci.karabuk.edu.tr>
2021-06-29 17:35:35 +0300

    İletşim admin sayfası eklendi. Arayüzü hazırlandı. Delete Fonksiyonu ve View fonksiyonu tamamlandı.

--- UlasBlog.WebUI/Controllers/AdminController.cs
+++ UlasBlog.WebUI/Controllers/AdminController.cs
@@ -3,15 +3,59 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UlasBlog.Data.Abstract;
 
 namespace UlasBlog.WebUI.Controllers
 {    
     public class AdminController : Controller
     {
+        private IUnitOfWork uow;
+        public AdminController(IUnitOfWork _uow)
+        {
+            uow = _uow;
+        }
         [Route("/Admin")]
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            try
+            {
+                var contact = uow.Contacts.GetAll();
+                return View(contact);
+            }
+            catch (Exception ex)
+            {
+                var error = ex.Message;
+                return View("_404NotFound");
+            }            
+        }
+        public IActionResult ContactDelete(int id)
+        {
+            var contact = uow.Contacts.Get(id);
+            if(contact !=null)
+            {
+                try
+                {
+                    uow.Contacts.Delete(contact);
+                    uow.SaveChanges();
+                    return Ok(id);
+                }
+                catch (Exception ex)
+                {
+                    var error = ex.Message;
+                    return BadRequest("İşlem Başarısız, Yöneticiye başvurun.");
+                }
+            }
+            else
+            {
+                return BadRequest("İlgili obje bulunamadı.");
+            }
+            
+        }
+
+
     }
 }
--- UlasBlog.WebUI/wwwroot/Home/js/Contact.js
+++ UlasBlog.WebUI/wwwroot/Home/js/Contact.js
@@ -1,4 +1,17 @@
-﻿$("#ContactForm").submit(function (event) {
+﻿$(document).on("click", ".open-editContactModal", function () {
+    var name = $(this).data('contact-name');
+    var id = $(this).data('contact-id');
+    var date = $(this).data('contact-date');
+    var email = $(this).data('contact-email');
+    var message = $(this).data('contact-message');
+    $(".modal-body #Name").val(name);
+    $(".modal-body #Id").val(id);
+    $(".modal-body #dateAdded").val(date);
+    $(".modal-body #Message").val(message);
+    $(".modal-body #Email").val(email);
+});
+
+$("#ContactForm").submit(function (event) {
     event.preventDefault();
     var form = $(this);
     var formData = new FormData(this);
@@ -12,14 +25,14 @@
         complete: function () {
             $("#ajax-loading").hide();
         },
-        success: function () {            
+        success: function () {
             document.getElementById("ContactForm").reset();
             iziToast.show({
                 title: 'Başarılı!',
                 message: 'Mesajınız iletildi.',
                 color: 'green', // blue, red, green, yellow
                 icon: 'fa fa-check'
-            }); 
+            });
         },
         error: function (errorMessage) {
             iziToast.show({
@@ -27,10 +40,56 @@
                 message: errorMessage.responseText,
                 color: 'red', // blue, red, green, yellow
                 icon: 'fa fa-times'
-            });  
+            });
         },
         cache: false,
         contentType: false,
         processData: false
     });
 });
+//yanıt gönder butonuna tıklandığında css değişiklikleri.
+function replySection() {    
+    document.getElementById('replySection').style.display = "block";
+    const btn = document.getElementById("replyBtn");
+    btn.classList.remove('btn-warning');
+    btn.classList.add('btn-success');
+    btn.innerText = "Yanıtı Gönder";
+}
+
+function Delete(url) {
+    event.preventDefault(); // prevent form submit
+    var form = event.target.form; // storing the form
+    var token = $('input[name="__RequestVerificationToken"]', form).val();
+    var data = { Id: $("#Id").val() };
+    var dataWithToken = $.extend(data, { '__RequestVerificationToken': token });
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                url: url,
+                type: "Delete",
+                data: $(this).serialize(),
+                beforeSend: function () {
+                    $("#ajax-loading").show();
+                },
+                complete: function () {
+                    $("#ajax-loading").hide();
+                },
+                success: function (Id) {
+                    console.log()
+                    var m = document.getElementById(Id);
+                    m.style.display = "none";
+                    toastr.success("İşlem Başarılı");
+                },
+                error: function (errorMessage) {
+                    toastr.error(errorMessage.responseText);
+                }
+            });
+        }
+    });
+}
