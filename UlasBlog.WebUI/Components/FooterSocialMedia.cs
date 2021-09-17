8da2ad5e7c9ca3059a4080b010b736fe46f70bfb
Fikri Ulaş Osmanağaoğlu <2014010213010@ogrenci.karabuk.edu.tr>
2021-09-17 11:52:19 +0300

    Anasayfa sosyal medya componentleri eklendi.

---
+++ UlasBlog.WebUI/Components/FooterSocialMedia.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UlasBlog.Data.Abstract;
+using UlasBlog.Entity;
+using UlasBlog.WebUI.Models;
+
+namespace UlasBlog.WebUI.Components
+{
+    public class FooterSocialMedia : ViewComponent
+    {
+        private IUnitOfWork uow;
+        public FooterSocialMedia(IUnitOfWork _unitOfWork)
+        {
+            uow = _unitOfWork;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var socialMedias = uow.Settings.GetAll()
+                .Where(i => i.Id == 5)
+                .Select(i => new Settings()
+                {
+                    Github = i.Github,
+                    Facebook = i.Facebook,
+                    Instagram = i.Instagram,
+                    Linkedin = i.Linkedin,
+                    Twitter = i.Twitter,
+                    Youtube = i.Youtube
+                }).FirstOrDefault();
+            return View(socialMedias);
+        }
+    }
+}
